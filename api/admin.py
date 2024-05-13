2c14a1903ba590f06ec21d67351b5c791effff39
shumilov <wumilovsergey@gmail.com>
2024-05-13 17:41:48 +0300

    rdy

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import TelegramUsers
+from .models import Urls
 
 
-admin.site.register(TelegramUsers)
+admin.site.register(Urls)
--- api/views.py
+++ api/views.py
@@ -1,30 +1,10 @@
 from django.views import View
 from django.shortcuts import render, redirect
-from .models import TelegramUsers
+from .models import  Urls
 
-
-from server.const import BOT_NAME
 
 class Main(View):
     def get(self, request):
-        return render(request, 'main.html')
+        urls = Urls.objects.all()
+        return render(request, 'main.html', {"urls": urls})
     
-class Login(View):
-    def get(self, request):
-        session_id = request.session["session_id"]
-        telegram_login_url = f"https://t.me/{BOT_NAME}?start={session_id}"
-        return redirect(telegram_login_url)
-    
-class Logout(View):
-    def get(self, request):
-        session_id = request.session["session_id"]
-        if TelegramUsers.objects.filter(session_id=session_id).exists():
-            user = TelegramUsers.objects.get(session_id=session_id)
-            user.session_id = "None"
-            user.save()
-
-        request.session["session_id"]=None
-        request.session["auth"]=None
-        request.session["name"]=None
-        request.session["org_select"]=None
-        return redirect("/")
